b857141b8270b72c33a8e9213ca94e0e3d361977
Ronghou Ha <ronghou.ha@outlook.com>
2019-01-15 13:53:46 +0800

    Fix Bugs for TMS

--- nav/mission_control/src/navi_manager/libNaviManager.cpp
+++ nav/mission_control/src/navi_manager/libNaviManager.cpp
@@ -518,7 +518,7 @@ int NaviManager::findPointFromThreeZone(double input_x,double input_y) {
   }
   //if (junction_list_.points.size() != 1) return -1;
 
-  double junction_range = 30;
+  double junction_range = 25; //25
 
   if (hypot((junction_list_.points[0].x - input_x),
     (junction_list_.points[0].y - input_y)) < junction_range) return 0;
