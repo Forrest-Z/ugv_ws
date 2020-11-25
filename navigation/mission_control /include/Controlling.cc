89be3c4266ac99a286ceb5cf91c8ecf6814ad240
maxt-yh <mxt0823@163.com>
2020-11-25 10:44:05 +0800

    Modify Navi Parameter

--- navigation/mission_control /include/Controlling.cc
+++ navigation/mission_control /include/Controlling.cc
@@ -25,18 +25,27 @@ void Controlling::ComputePurePursuitCommand(geometry_msgs::Point32 Goal_Route, g
   double u_turn_speed_scale = 0; 
   double u_turn_rotation_scale = 0.8;
   double rotation_threshold = 0.78;
+  double face_rotation_threshold = 1.2;
 
   double goal_plan_distance = hypot(Goal_Plan.x,Goal_Plan.y);
   double goal_plan_yaw = atan2(Goal_Plan.y,Goal_Plan.x);
   double faceing_angle = atan2(Goal_Route.y,Goal_Route.x);
 
   max_linear_acceleration_ = temp_max_linear_acceleration_;
+
+  if(fabs(faceing_angle) > face_rotation_threshold) {
+    Pp_command.linear.x =  speed_scale_ * u_turn_speed_scale;
+    Pp_command.angular.z = rotation_scale_ * u_turn_rotation_scale 
+                            * (faceing_angle/fabs(faceing_angle));
+    max_linear_acceleration_ = temp_max_linear_acceleration_ * 8;
+    return;
+  }
   
   if(fabs(goal_plan_yaw) > rotation_threshold) {
     Pp_command.linear.x =  speed_scale_ * u_turn_speed_scale;
     Pp_command.angular.z = rotation_scale_ * u_turn_rotation_scale 
                             * (goal_plan_yaw/fabs(goal_plan_yaw));
-    max_linear_acceleration_ = temp_max_linear_acceleration_ * 4;
+    max_linear_acceleration_ = temp_max_linear_acceleration_ * 8;
   } else {
     Pp_command.linear.x =  speed_scale_ * goal_plan_distance;
     Pp_command.angular.z = rotation_scale_ * goal_plan_yaw;
